3640ba22235aae6cd66856938f134582fcdc6ec4
atulkuchil@gmail.com <atulkuchil@gmail.com>
2020-04-22 21:58:46 +0530

    Added mydisplay and draw_pyraminx function for easy access while rotating

--- Pyraminx/pyraminx.cpp
+++ Pyraminx/pyraminx.cpp
@@ -4,7 +4,9 @@
 #include<math.h>
 
 GLfloat v[4][3] = {{0.0,45.0,0.0},{-12.245,25.0,7.07},{12.245,25.0,7.07},{0.0,25.0,-14.14}};
+GLfloat colors[4][3] = {{1.0,0.0,0.0}, {0.0,0.0,1.0}, {0.0,1.0,0.0}, {1.0,1.0,0.0}};
 int count =0;
+
 class pyraminx{
 	public:
 		GLfloat vertices[4][3] = {0.0};
@@ -52,14 +54,14 @@ void tetrahedron(){
 		pyramid[0].vertices[3][1] = v[3][1];
 		pyramid[0].vertices[3][2] = v[3][2];
 
-		glColor3f(1.0,0.0,0.0);
+		glColor3fv(colors[0]);
 		triangle(v[0],v[1],v[2]);
-		glColor3f(0.0,1.0,0.0);
-		triangle(v[3],v[1],v[2]);
-		glColor3f(1.0,1.0,0.0);
+		glColor3fv(colors[1]);
+		triangle(v[0],v[2],v[3]);
+		glColor3fv(colors[2]);
 		triangle(v[0],v[1],v[3]);
-		glColor3f(0.0,0.0,1.0);
-		triangle(v[0],v[3],v[2]);
+		glColor3fv(colors[3]);
+		triangle(v[1],v[3],v[2]);
 
 		inner_pyramid[i].vertices[0][0][0] = pyramid[i].vertices[1][0];
 		inner_pyramid[i].vertices[0][0][1] = pyramid[i].vertices[1][1];
@@ -101,13 +103,13 @@ void tetrahedron(){
 		inner_pyramid[i].vertices[3][2][1] = inner_pyramid[i].vertices[2][2][1];
 		inner_pyramid[i].vertices[3][2][2] = inner_pyramid[i].vertices[2][2][2];
 
-		glColor3f(1.0,0.0,0.0);
+		glColor3fv(colors[0]);
 		triangle(inner_pyramid[i].vertices[0][0],inner_pyramid[i].vertices[0][1],inner_pyramid[i].vertices[0][2]);
-		glColor3f(0.0,0.0,1.0);
+		glColor3fv(colors[1]);
 		triangle(inner_pyramid[i].vertices[1][0],inner_pyramid[i].vertices[1][1],inner_pyramid[i].vertices[1][2]);
-		glColor3f(1.0,1.0,0.0);
+		glColor3fv(colors[2]);
 		triangle(inner_pyramid[i].vertices[2][0],inner_pyramid[i].vertices[2][1],inner_pyramid[i].vertices[2][2]);
-		glColor3f(0.0,1.0,0.0);
+		glColor3fv(colors[3]);
 		triangle(inner_pyramid[i].vertices[3][0],inner_pyramid[i].vertices[3][1],inner_pyramid[i].vertices[3][2]);
 	}
 
@@ -129,14 +131,14 @@ void tetrahedron(){
 		pyramid[i].vertices[3][1] = v[i][1] - 20;
 		pyramid[i].vertices[3][2] = v[i][2] - 14.14;
 
-		glColor3f(1.0,0.0,0.0);
+		glColor3fv(colors[0]);
 		triangle(pyramid[i].vertices[0],pyramid[i].vertices[1],pyramid[i].vertices[2]);
-		glColor3f(0.0,1.0,0.0);
-		triangle(pyramid[i].vertices[3],pyramid[i].vertices[1],pyramid[i].vertices[2]);
-		glColor3f(1.0,1.0,0.0);
-		triangle(pyramid[i].vertices[0],pyramid[i].vertices[1],pyramid[i].vertices[3]);
-		glColor3f(0.0,0.0,1.0);
+		glColor3fv(colors[1]);
 		triangle(pyramid[i].vertices[0],pyramid[i].vertices[3],pyramid[i].vertices[2]);
+		glColor3fv(colors[2]);
+		triangle(pyramid[i].vertices[0],pyramid[i].vertices[1],pyramid[i].vertices[3]);
+		glColor3fv(colors[3]);
+		triangle(pyramid[i].vertices[3],pyramid[i].vertices[1],pyramid[i].vertices[2]);
 
 		inner_pyramid[i].vertices[0][0][0] = pyramid[i].vertices[1][0];
 		inner_pyramid[i].vertices[0][0][1] = pyramid[i].vertices[1][1];
@@ -178,13 +180,13 @@ void tetrahedron(){
 		inner_pyramid[i].vertices[3][2][1] = inner_pyramid[i].vertices[2][2][1];
 		inner_pyramid[i].vertices[3][2][2] = inner_pyramid[i].vertices[2][2][2];
 
-		glColor3f(1.0,0.0,0.0);
+		glColor3fv(colors[0]);
 		triangle(inner_pyramid[i].vertices[0][0],inner_pyramid[i].vertices[0][1],inner_pyramid[i].vertices[0][2]);
-		glColor3f(0.0,0.0,1.0);
+		glColor3fv(colors[1]);
 		triangle(inner_pyramid[i].vertices[1][0],inner_pyramid[i].vertices[1][1],inner_pyramid[i].vertices[1][2]);
-		glColor3f(1.0,1.0,0.0);
+		glColor3fv(colors[2]);
 		triangle(inner_pyramid[i].vertices[2][0],inner_pyramid[i].vertices[2][1],inner_pyramid[i].vertices[2][2]);
-		glColor3f(0.0,1.0,0.0);
+		glColor3fv(colors[3]);
 		triangle(inner_pyramid[i].vertices[3][0],inner_pyramid[i].vertices[3][1],inner_pyramid[i].vertices[3][2]);
 	}
 
@@ -203,14 +205,14 @@ void tetrahedron(){
 		pyramid[i*2+2].vertices[3][1] = pyramid[i].vertices[i][1] - 20;
 		pyramid[i*2+2].vertices[3][2] = pyramid[i].vertices[i][2] - 14.14;
 
-		glColor3f(1.0,0.0,0.0);
+		glColor3fv(colors[0]);
 		triangle(pyramid[i*2+2].vertices[0],pyramid[i*2+2].vertices[1],pyramid[i*2+2].vertices[2]);
-		glColor3f(0.0,1.0,0.0);
-		triangle(pyramid[i*2+2].vertices[3],pyramid[i*2+2].vertices[1],pyramid[i*2+2].vertices[2]);
-		glColor3f(1.0,1.0,0.0);
-		triangle(pyramid[i*2+2].vertices[0],pyramid[i*2+2].vertices[1],pyramid[i*2+2].vertices[3]);
-		glColor3f(0.0,0.0,1.0);
+		glColor3fv(colors[1]);
 		triangle(pyramid[i*2+2].vertices[0],pyramid[i*2+2].vertices[3],pyramid[i*2+2].vertices[2]);
+		glColor3fv(colors[2]);
+		triangle(pyramid[i*2+2].vertices[0],pyramid[i*2+2].vertices[1],pyramid[i*2+2].vertices[3]);
+		glColor3fv(colors[3]);
+		triangle(pyramid[i*2+2].vertices[3],pyramid[i*2+2].vertices[1],pyramid[i*2+2].vertices[2]);
 
 		int j = i+1;
 		if(i == 3){
@@ -230,35 +232,73 @@ void tetrahedron(){
 		pyramid[i*2+3].vertices[3][1] = pyramid[i].vertices[j][1] - 20;
 		pyramid[i*2+3].vertices[3][2] = pyramid[i].vertices[j][2] - 14.14;
 
-		glColor3f(1.0,0.0,0.0);
+		glColor3fv(colors[0]);
 		triangle(pyramid[i*2+3].vertices[0],pyramid[i*2+3].vertices[1],pyramid[i*2+3].vertices[2]);
-		glColor3f(0.0,1.0,0.0);
-		triangle(pyramid[i*2+3].vertices[3],pyramid[i*2+3].vertices[1],pyramid[i*2+3].vertices[2]);
-		glColor3f(1.0,1.0,0.0);
-		triangle(pyramid[i*2+3].vertices[0],pyramid[i*2+3].vertices[1],pyramid[i*2+3].vertices[3]);
-		glColor3f(0.0,0.0,1.0);
+		glColor3fv(colors[1]);
 		triangle(pyramid[i*2+3].vertices[0],pyramid[i*2+3].vertices[3],pyramid[i*2+3].vertices[2]);
+		glColor3fv(colors[2]);
+		triangle(pyramid[i*2+3].vertices[0],pyramid[i*2+3].vertices[1],pyramid[i*2+3].vertices[3]);
+		glColor3fv(colors[3]);
+		triangle(pyramid[i*2+3].vertices[3],pyramid[i*2+3].vertices[1],pyramid[i*2+3].vertices[2]);
 	}
 	count = 0;
 }
 
+void draw_pyraminx(){
+	for(int pyramid_count = 0; pyramid_count < 10; pyramid_count++){
+		glColor3fv(colors[0]);
+		triangle(pyramid[pyramid_count].vertices[0],pyramid[pyramid_count].vertices[1],pyramid[pyramid_count].vertices[2]);
+		glColor3fv(colors[1]);
+		triangle(pyramid[pyramid_count].vertices[0],pyramid[pyramid_count].vertices[2],pyramid[pyramid_count].vertices[3]);
+		glColor3fv(colors[2]);
+		triangle(pyramid[pyramid_count].vertices[0],pyramid[pyramid_count].vertices[3],pyramid[pyramid_count].vertices[1]);
+		glColor3fv(colors[3]);
+		triangle(pyramid[pyramid_count].vertices[1],pyramid[pyramid_count].vertices[2],pyramid[pyramid_count].vertices[3]);
+	}
+	for(int inner_pyraminx_count =0; inner_pyraminx_count < 4; inner_pyraminx_count++){
+		glColor3fv(colors[0]);
+		triangle(inner_pyramid[inner_pyraminx_count].vertices[0][0],inner_pyramid[inner_pyraminx_count].vertices[0][1],inner_pyramid[inner_pyraminx_count].vertices[0][2]);
+		glColor3fv(colors[1]);
+		triangle(inner_pyramid[inner_pyraminx_count].vertices[1][0],inner_pyramid[inner_pyraminx_count].vertices[1][1],inner_pyramid[inner_pyraminx_count].vertices[1][2]);
+		glColor3fv(colors[2]);
+		triangle(inner_pyramid[inner_pyraminx_count].vertices[2][0],inner_pyramid[inner_pyraminx_count].vertices[2][1],inner_pyramid[inner_pyraminx_count].vertices[2][2]);
+		glColor3fv(colors[3]);
+		triangle(inner_pyramid[inner_pyraminx_count].vertices[3][0],inner_pyramid[inner_pyraminx_count].vertices[3][1],inner_pyramid[inner_pyraminx_count].vertices[3][2]);
+	}
+}
+
 static GLfloat theta[] = {0.0,0.0,0.0};
 static GLint axis = 2;
+int flag_display = 0;
 
 
-static GLdouble viewer[]= {0.0, 0.0, 5.0}; /* initial viewer location */
-
-void display(void){
+void mydisplay(void){
 	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
     glLoadIdentity();
-	// printf("theta[0]: %f, theta[1]: %f, theta[2]: %f\n",thetaz[0],thetaz[1],thetaz[2]);
     glRotatef(theta[0],1.0,0.0,0.0);
     glRotatef(theta[1],0.0,1.0,0.0);
     glRotatef(theta[2],0.0,0.0,1.0);
-    tetrahedron();
-	// draw_inner_pyramid();
+    draw_pyraminx();
 	glFlush();
 	glutSwapBuffers();
+}
+
+
+void display(void){
+	if(flag_display == 1){
+		mydisplay();
+	}
+	else{
+		flag_display == 1;
+		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+    	glLoadIdentity();
+    	glRotatef(theta[0],1.0,0.0,0.0);
+    	glRotatef(theta[1],0.0,1.0,0.0);
+    	glRotatef(theta[2],0.0,0.0,1.0);
+    	tetrahedron();
+		glFlush();
+		glutSwapBuffers();
+	}
 }
 
 void mouse(int btn, int state, int x, int y){
@@ -274,19 +314,11 @@ void mouse(int btn, int state, int x, int y){
 	if( theta[axis] > 360.0 )
         theta[axis] -= 360.0;
     printf("theta[axis]: %f\n",theta[axis]);
-	display();
+	mydisplay();
 }
 
 // void keys(unsigned char key, int x, int y)
 // {
-// /* Use x, X, y, Y, z, and Z keys to move viewer */
-//     if(key == 'x') viewer[0]-= 1.0;
-//     if(key == 'X') viewer[0]+= 1.0;
-//     if(key == 'y') viewer[1]-= 1.0;
-//     if(key == 'Y') viewer[1]+= 1.0;
-//     if(key == 'z') viewer[2]-= 1.0;
-//     if(key == 'Z') viewer[2]+= 1.0;
-//     display();
 // }
 
 void myReshape(int w, int h){
